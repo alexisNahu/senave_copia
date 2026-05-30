ac0ef2fface246dad60b77cc3065990d7af03962
anig <alexisnahuelidoyaga@gmail.com>
2026-05-30 12:39:30 -0300

    cambio de estilos

---
+++ src/core/mocks/agroquimicos.ts
@@ -0,0 +1,261 @@
+export const formularios_entidad_comercial = [
+    {
+        title: 'Departamento de Control y Evaluación de Insumos Agrícolas (DCEI)',
+        url: '#dcei'
+    },
+    {
+        title: 'Departamento de Evaluación y Mitigación de Riesgos de los Plaguicidas (DEyMRP)',
+        url: '#deymrp'
+    },
+    {
+        title: 'Departamento de Divulgación y Manejo Seguro de los Plaguicidas (DDyMSP)',
+        url: '#ddymsp'
+    },
+    {
+        title: 'Comisión Técnica Evaluadora (CTE)',
+        url: '#cte'
+    }
+];
+
+export const formularios_habilitacion_depositos = [
+    {
+        title: 'Nota proforma. Solicitud de Habilitación de Depósitos',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/NOTA PROFORMA HABILITACION DE DEPOSITO DE PRODUCTOS FITOSANITARIOS.docx'
+    },
+    {
+        title: 'Formulario de verificación de depósitos de productos fitosanitarios',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/FORMULARIO DE VERIFICACION DE DEPOSITOS DE PRODUCTOS FITOSANITARIOS.docx'
+    }
+];
+
+export const requisitos_laboratorios_reconocidos = [
+    {
+        title: 'Requisitos. Registro y renovación de laboratorios (internacionales)',
+        url: '#'
+    }
+];
+
+export const formularios_laboratorios_reconocidos = [
+    {
+        title: 'Nota proforma. Reconocimiento de laboratorio',
+        url: '#'
+    },
+    {
+        title: 'Solicitud de registro de laboratorios reconocidos',
+        url: '#'
+    }
+];
+
+export const requisitos_laboratorios_habilitados = [
+    {
+        title: 'Requisitos. Registro y renovación de laboratorios (nacionales)',
+        url: '#'
+    }
+];
+
+export const formularios_laboratorios_habilitados = [
+    {
+        title: 'Nota proforma. Laboratorios habilitados',
+        url: '#'
+    },
+    {
+        title: 'Solicitud de registro de laboratorios habilitados',
+        url: '#'
+    }
+];
+
+export const registro_laboratorios_toxicologia = [
+    {
+        title: 'Requisitos. Registro de laboratorios habilitados (nacionales) de toxicología y ecotoxicología',
+        url: '#'
+    },
+    {
+        title: 'Solicitud de registro de laboratorios habilitados de acuerdo con BPL de la OCDE',
+        url: '#'
+    }
+];
+
+export const profesionales = [
+    {
+        title: 'Trámite de registro de profesionales',
+        url: '#'
+    },
+    {
+        title: 'Nota de solicitud de registro/renovación de asesor técnico',
+        url: '#'
+    }
+];
+
+
+export const pilotos = [
+    {
+        title: 'Requisitos. Piloto aviador agrícola de aeronave tripulada- PAAG',
+        url: '#'
+    },
+    {
+        title: 'Requisitos. Piloto de aeronave pilotada a distancia- P-RPA',
+        url: '#'
+    },
+    {
+        title: 'Marco Normativo',
+        url: '#'
+    }
+];
+
+export const documentos_generales = [
+    {
+        title: 'FOR-CTE-048 – TABLA DE TIPO DE FORMULACIÓN VS PROPIEDADES FISICO QUIMICAS PARA PRODUCTO FORMULADO',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/FOR-CTE-048%20-%20TABLA%20DE%20TIPO%20DE%20FORMULACION%20VS%20PROPIEDADES%20FISICO%20QUIMICAS%20PARA%20PRODUCTO%20FORMULADO%20(1).xlsx'
+    },
+    {
+        title: 'FOR-CTE-049 – TABLA DE SUSTANCIA ACTIVA GRADO TÉCNICO Y TÉCNICO CONCENTRADO',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/49_FOR-CTE-049%20-%20TABLA%20DE%20SUSTANCIA%20ACTIVA%20GRADO%20TECNICO%20Y%20TECNICO%20CONCENTRADO.docx'
+    },
+    {
+        title: 'FOR-CTE-054 – SOLICITUD PARA PAGO DE SERVICIOS DE REGISTRO DE PRODUCTOS FITOSANITARIOS',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/54_FOR-CTE-054%20SOLICITUD%20PARA%20PAGO%20DE%20SERVICIOS%20DE%20REGISTRO%20DE%20PRODUCTOS%20FITOSANITARIOS.docx'
+    },
+    {
+        title: 'FOR-CTE-055 – Nota proforma para solicitud SOPORTE GRADO TÉCNICO Y GRADO TÉCNICO',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/55_FOR-CTE-055 - Nota proforma para solicitud SOPORTE GRADO TECNICO Y GRADO TECNICO.docx'
+    },
+    {
+        title: 'FOR-CTE-056 – Nota proforma para solicitud de PRODUCTOS FITOSANITARIOS',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/56_FOR-CTE-056 - Nota proforma para solicitud de PRODUCTOS FITOSANITARIOS.docx'
+    },
+    {
+        title: 'FOR-CTE-058 – SOLICITUD DE REGISTRO DE PRODUCTOS FITOSANITARIOS DE USO AGRICOLA Y AFINES',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/58_FOR-CTE-058 -SOLICITUD DE REGISTRO DE PRODUCTOS FITOSANITARIOS DE USO AGRICOLA Y AFINES v02.docx'
+    },
+    {
+        title: 'FOR-CTE-004 – CARTA DE COMPROMISO PARA CERTIFICADO DE ORIGEN',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/4_FOR-CTE-004-CARTA-DE-COMPROMISO-PARA-CERTIFICADO-DE-ORIGEN.docx'
+    },
+    {
+        title: 'CIRCULAR N° 01-24 – NORMAS DE USO SESAMO',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/CIRCULAR-N-01-24-NORMAS-DE-USO-SESAMO.pdf'
+    }
+];
+
+export const definitivo_producto_formulado = [
+    {
+        title: 'REQUISITOS Y PROCESOS PARA REGISTRO DE PRODUCTOS FORMULADOS',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/2022/REGISTRO%20DEFINITIVO.REQUISITOS%20Y%20PROCESOS%20PARA%20REGISTRO%20DE%20PRODUCTOS%20FORMULADOS-%20REGISTRO%20GRADO%20TECNICO.pdf'
+    },
+    {
+        title: 'FOR-CTE-009 – CHECK LIST PARA REQUISITOS TÉCNICOS DE REGISTRO DE PRODUCTO FORMULADO EN BASE A SUSTANCIA ACTIVA QUIMICA GRADO TÉCNICO EQUIVALENTE',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/9_FOR-CTE-009%20CHECK%20LIST%20PARA%20REQUISITOS%20TECNICOS%20DE%20REGISTRO%20DE%20PRODUCTO%20FORMULADO%20EN%20BASE%20A%20SUSTANCIA%20ACTIVA%20QUIMICA%20GRADO%20TECNICO%20EQUIVALENTE.docx'
+    }
+];
+
+export const presentacion_cuerpo_3 = [
+    {
+        title: ' FOR-CTE-063 ANEXO IV RESUMEN ESTUDIOS SOLICITADOS EN EL CUERPO III – CUERPO TECNICO PARA EL REGISTRO DE PRODUCTOS FORMULADOS EN BASE A SUSTANCIAS ACTIVAS QUÍMICA GRADO TÉCNICO EQUIVALENTE\n', url: '#'
+    }
+];
+
+export const forma_digital = [
+    {
+        title: ' FOR-CTE-067 – ANEXO VIII INDICE ESTUDIOS SOLICITADOS EN EL CUERPO III – CUERPO TECNICO PARA EL REGISTRO DE PRODUCTO FORMULADO EN BASE A SUSTANCIA ACTIVA QUIMICA GRADO TECNICO EQUIVALENTE',
+        url: '#'
+    }
+]
+
+export const sustancia_activa = [
+    {title: 'REQUISITOS Y PROCESOS PARA REGISTRO GRADO TECNICO/SOPORTE GRADO TÉCNICO', url: '#'},
+    {title: ' FOR-CTE-050 – CHECK LIST PARA REQUISITOS TÉCNICOS DE REGISTRO DE SUSTANCIA ACTIVA QUIMICA GRADO TÉCNICO EQUIVALENTE', url: '#'}
+]
+
+export const presentacion_cuerpo_32 = [
+    {title: 'FOR-CTE-062 – ANEXO III RESUMEN-ESTUDIOS SOLICITADOS EN EL CUERPO III – CUERPO TECNICO-PARA EL REGISTRO DE SUSTANCIA ACTIVA QUIMICA GRADO TÉCNICO EQUIVALENTE', url: '#'}
+]
+
+export const control_biologico_microbiano = [
+    {
+        title: 'FOR-CTE-068 – SOLICITUD DE REGISTRO DE PRODUCTOS ACBM',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/68_FOR-CTE-068 SOLICITUD DE REGISTRO DE PRODUCTOS ACBM -v02.docx'
+    },
+    {
+        title: 'FOR-CTE-006 CHECK LIST BIOLOGICOS',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/6_FOR-CTE-006%20CHECK%20LIST%20BIOLOGICOS.docx'
+    }
+];
+
+export const trato_diferenciado = [
+    {
+        title: 'REQUISITOS Y PROCESOS PARA REGISTRO DE PRODUCTOS FORMULADOS/ MATERIA PRIMA DE INSUMOS AGRÍCOLAS POR TRATO DIFERENCIADO',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/2022/REGISTRO%20DEFINITIVO.REQUISITOS%20Y%20PROCESOS%20PARA%20REGISTRO%20POR%20TRATO%20DIFERENCIADO.pdf'
+    },
+    {
+        title: 'FOR-CTE-057 – CHECK LIST PARA REQUISITOS TÉCNICOS DE REGISTRO DE INSUMOS AGRICOLAS POR TRATO DIFERENCIADO',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/57_FOR-CTE-057%20CHECK%20LIST%20PARA%20REQUISITOS%20TECNICOS%20DE%20REGISTRO%20DE%20INSUMOS%20AGRICOLAS%20POR%20TRATO%20DIFERENCIADO%20(1).docx'
+    },
+    {
+        title: 'FOR-CTE-059 – CHECK LIST PARA REQUISITOS TÉCNICOS DE REGISTRO DE INSUMOS AGRICOLAS TRATO DIFERENCIADO MATERIA PRIMA',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/59_FOR-CTE-059%20CHECK%20LIST%20PARA%20REQUISITOS%20TECNICOS%20DE%20REGISTRO%20DE%20INSUMOS%20AGRICOLAS%20TRATO%20DIFERENCIADO%20MATERIA%20PRIMA.docx'
+    }
+];
+
+export const experimental_producto_formulado = [
+    {
+        title: 'FOR-CTE-053 – CHECK LIST PARA REQUISITOS TÉCNICOS DE REGISTRO EXPERIMENTAL DE PRODUCTO FORMULADO EN BASE A SUSTANCIA ACTIVA QUIMICA Y BIOQUIMICA NUEVA SIN ANTECEDENTE DE USO',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/53_FOR-CTE-053%20CHECK%20LIST%20PARA%20REQUISITOS%20TECNICOS%20DE%20REGISTRO%20EXPERIMENTAL%20DE%20PRODUCTO%20FORMULADO%20EN%20BASE%20A%20SUSTANCIA%20ACTIVA%20QUIMICA%20Y%20BIOQUIMICA%20NUEVA%20SIN%20ANTECEDENTE%20DE%20USO.docx'
+    }
+];
+
+export const experimental_sustancia_activa = [
+    {
+        title: 'FOR-CTE-052 – Check List Experimental GT Nueva',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/52_FOR-CTE-052%20Check%20List%20Experimental%20GT%20Nueva.docx'
+    },
+    {
+        title: 'FOR-CTE-060 – ANEXO I RESUMEN. ESTUDIOS SOLICITADOS EN EL CUERPO III (Forma Física)',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/60_FOR-CTE-060%20ANEXO%20I%20RESUMEN.%20ESTUDIOS%20SOLICITADOS%20EN%20EL%20CUERPO%20III.docx'
+    },
+    {
+        title: 'FOR-CTE-064 ANEXO V INDICE – ESTUDIOS SOLICITADOS EN EL CUERPO III – CUERPO TECNICO PARA EL REGISTRO DE SUSTANCIA ACTIVA QUIMICA Y BIOQUIMICA NUEVA SIN ANTECEDENTES DE USO (Forma Digital)',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/64_FOR-CTE-064-ANEXO-V-INDICE-ESTUDIOS-SOLICITADOS-EN-EL-CUERPO-III-CUERPO-TECNICO-PARA-EL-REGISTRO-DE-SUSTANCIA-ACTIVA-QUIMICA-Y-BIOQUIMICA-NUEVA-SIN-ANTECEDENTES-DE-USO.docx'
+    }
+];
+
+export const informaciones_generales = [
+    {
+        title: 'ETIQUETADOS DE PRODUCTOS FITOSANITARIOS',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/ETIQUETADOS%20DE%20PRODUCTOS%20FITOSANITARIOS%20.pdf'
+    },
+    {
+        title: 'REQUISITOS TRATO DIFERENCIADO',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/REGISTRO%20DE%20INSUMOS%20AGRICOLAS%20POR%20TRATO%20DIFERENCIADO.pdf'
+    }
+];
+
+export const solicitudes_pos_registros = [
+    {
+        title: 'REQUISITOS DE ORIGEN ADICIONAL',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/ORIGEN%20ADICIONAL-FABRICANTE%20ADICIONAL.pdf'
+    },
+    {
+        title: 'REQUISITOS DE CAMBIO DE FABRICANTE',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/CAMBIO%20DE%20FABRICANTE.pdf'
+    },
+    {
+        title: 'REQUISITOS DE CAMBIO DE CLASIFICACIÓN TOXICOLÓGICA',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/CAMBIO%20DE%20CLASIFICACION%20TOXICOLCOA.pdf'
+    },
+    {
+        title: 'REQUISITOS DE AMPLIACIÓN DE USO',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/AMPLIACION%20DE%20USO%20-%20CULTIVO%20CLASE%20DE%20USO.pdf'
+    },
+    {
+        title: 'TRANSFERENCIA DE REGISTROS',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/TRANSFERENCIA%20DE%20REGISTRO.pdf'
+    },
+    {
+        title: 'CAMBIO DE CONCENTRACIÓN',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/CAMBIO%20DE%20CONCENTRACION.pdf'
+    },
+    {
+        title: 'CANCELACIÓN DE REGISTROS',
+        url: 'https://www.senave.gov.py/docs/agroquimicos/registros-agroquimicos/CANCELACION%20DE%20REGISTROS.pdf'
+    }
+];
